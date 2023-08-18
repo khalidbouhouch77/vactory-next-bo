4d4dc56283718bc72ef498af213471ff1aee9e37
Mohamed Chourafa <chourafamohamed@gmail.com>
2023-08-18 15:20:02 +0200

    creating form

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SidebarNav } from "@/components/sidebar-nav";
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "@/components/ui/separator";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,10 +32,10 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="hidden space-y-6 p-10 pb-16 md:block">
           <div className="space-y-0.5">
-            <h2 className="text-2xl font-bold tracking-tight">VACTORY V4 - NEXT BACK OFFICE</h2>
-            <p className="text-muted-foreground">
-              Back office front
-            </p>
+            <h2 className="text-2xl font-bold tracking-tight">
+              VACTORY V4 - NEXT BACK OFFICE
+            </h2>
+            <p className="text-muted-foreground">Back office front</p>
           </div>
           <Separator className="my-6" />
           <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
--- app/translate/page.tsx
+++ app/translate/page.tsx
@@ -1,4 +1,80 @@
+"use client";
+import { useState } from "react";
+import { Separator } from "@/components/ui/separator";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+interface ListItem {
+  key: string;
+  value: string;
+}
+
 export default function Translate() {
-    return <>Translate</>
-  }
-  
+  const [items, setItems] = useState<ListItem[]>([]);
+  const [keyInput, setKeyInput] = useState("");
+  const [valueInput, setValueInput] = useState("");
+
+  const handleAdd = () => {
+    if (keyInput && valueInput) {
+      const newItem: ListItem = { key: keyInput, value: valueInput };
+      setItems([...items, newItem]);
+      setKeyInput("");
+      setValueInput("");
+    }
+    console.log(items);
+  };
+
+  const handleDelete = (index: number) => {
+    const updatedItems = items.filter((_, i) => i !== index);
+    setItems(updatedItems);
+  };
+
+  return (
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">SUBMIT</h3>
+        <p className="text-sm text-muted-foreground">
+          Enter to each key a value
+        </p>
+      </div>
+      <Separator />
+
+      {/* <div className="flex w-full max-w-sm items-center space-x-2">
+        <Input type="text" placeholder="key" />
+        <Input type="text" placeholder="value" />
+        <button type="submit">+</button>
+  </div>*/}
+      <div className="flex flex-col gap-3">
+        <Button variant="outline" onClick={handleAdd} className="self-end">
+          Add
+        </Button>
+        <div className="flex w-full max-w-sm items-center space-x-2">
+          <Input
+            type="text"
+            placeholder="key"
+            value={keyInput}
+            onChange={(e) => setKeyInput(e.target.value)}
+          />
+          <Input
+            type="text"
+            placeholder="value"
+            value={valueInput}
+            onChange={(e) => setValueInput(e.target.value)}
+          />
+        </div>
+      </div>
+      {items.map((item, index) => (
+        <div key={index} className="flex  items-center space-x-2">
+          <div className="flex w-full max-w-sm items-center space-x-2">
+            <Input value={item.key} />
+            <Input value={item.value} />
+          </div>
+          <Button type="button" onClick={() => handleDelete(index)}>
+            Delete
+          </Button>
+        </div>
+      ))}
+      <Button type="button">Save On MongoDb</Button>
+    </div>
+  );
+}
